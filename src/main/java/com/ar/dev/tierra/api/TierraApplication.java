3187df78e77d29d447c9910c7287e8ea3c25f950
Paulo Galdo <hades_54st@hotmail.com>
2016-02-27 22:57:38 -0300

    Comntarios eliminados

--- src/main/java/com/ar/dev/tierra/api/TierraApplication.java
+++ src/main/java/com/ar/dev/tierra/api/TierraApplication.java
@@ -5,39 +5,19 @@
  */
 package com.ar.dev.tierra.api;
 
-import javax.servlet.MultipartConfigElement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  *
  * @author PauloGaldo
  */
 @SpringBootApplication
-//@Configuration
-//@ComponentScan
 @EnableAutoConfiguration
 public class TierraApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TierraApplication.class, args);
     }
-
-//    @Bean
-//    public MultipartConfigElement multipartConfigElement() {
-//        return new MultipartConfigElement("");
-//    }
-//
-//    @Bean
-//    public MultipartResolver multipartResolver() {
-//        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
-//        resolver.setMaxUploadSize(1000000);
-//        return resolver;
-//    }
 }
