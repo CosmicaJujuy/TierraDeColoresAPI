4ac3fd602447026fd969f8f38b86bbc8a36b34ce
Paulo Galdo <hades_54st@hotmail.com>
2016-06-23 04:12:55 -0300

    interface nota de credito

---
+++ src/main/java/com/ar/dev/tierra/api/dao/NotaCreditoDAO.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ar.dev.tierra.api.dao;
+
+import com.ar.dev.tierra.api.model.NotaCredito;
+import java.util.List;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author PauloGaldo
+ */
+@Component
+@Repository
+public interface NotaCreditoDAO {
+
+    public List<NotaCredito> getAll();
+
+    public void add(NotaCredito notaCredito);
+
+    public void update(NotaCredito notaCredito);
+
+    public void delete(NotaCredito notaCredito);
+
+    public List<NotaCredito> searchByIdCliente(int idCliente);
+
+    public List<NotaCredito> getDaily();
+
+    public List<NotaCredito> getMonth();
+
+}
