fb5c287a6817a6c52f620e50be285a47497557a5
Paulo Galdo <hades_54st@hotmail.com>
2016-06-23 11:19:59 -0300

    implement para detalles de nota de credito agregado

---
+++ src/main/java/com/ar/dev/tierra/api/dao/impl/DetalleNotaCreditoDAOImpl.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ar.dev.tierra.api.dao.impl;
+
+import com.ar.dev.tierra.api.dao.DetalleNotaCreditoDAO;
+import com.ar.dev.tierra.api.model.DetalleNotaCredito;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author PauloGaldo
+ */
+@Repository
+@Transactional
+public class DetalleNotaCreditoDAOImpl implements DetalleNotaCreditoDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<DetalleNotaCredito> getByNotaCredito(int idNotaCredito) {
+        Criteria criteria = getSession().createCriteria(DetalleNotaCredito.class);
+        Criteria notaCredito = criteria.createCriteria("notaCredito");
+        notaCredito.add(Restrictions.eq("idNotaCredito", idNotaCredito));
+        criteria.addOrder(Order.desc("idDetalleNotaCredito"));
+        List<DetalleNotaCredito> list = criteria.list();
+        return list;
+    }
+
+    @Override
+    public void add(DetalleNotaCredito detalleNotaCredito) {
+        getSession().save(detalleNotaCredito);
+    }
+
+    @Override
+    public void update(DetalleNotaCredito detalleNotaCredito) {
+        getSession().update(detalleNotaCredito);
+    }
+
+    @Override
+    public void delete(DetalleNotaCredito detalleNotaCredito) {
+        getSession().delete(detalleNotaCredito);
+    }
+
+}
