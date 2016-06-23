8c324fe7c5b0907d25ebf1460593a869c491b55c
Paulo Galdo <hades_54st@hotmail.com>
2016-06-23 11:20:14 -0300

    añadir y actualizar agregados

--- src/main/java/com/ar/dev/tierra/api/controller/NotaCreditoController.java
+++ src/main/java/com/ar/dev/tierra/api/controller/NotaCreditoController.java
@@ -5,13 +5,22 @@
  */
 package com.ar.dev.tierra.api.controller;
 
+import com.ar.dev.tierra.api.dao.DetalleNotaCreditoDAO;
 import com.ar.dev.tierra.api.dao.NotaCreditoDAO;
+import com.ar.dev.tierra.api.dao.UsuariosDAO;
+import com.ar.dev.tierra.api.model.DetalleNotaCredito;
+import com.ar.dev.tierra.api.model.JsonResponse;
 import com.ar.dev.tierra.api.model.NotaCredito;
+import com.ar.dev.tierra.api.model.Usuarios;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +37,12 @@ public class NotaCreditoController implements Serializable {
     @Autowired
     NotaCreditoDAO notaCreditoDAO;
 
+    @Autowired
+    UsuariosDAO usuariosDAO;
+
+    @Autowired
+    DetalleNotaCreditoDAO detalleNotaCreditoDAO;
+
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public ResponseEntity<?> getAll() {
         List<NotaCredito> list = notaCreditoDAO.getAll();
@@ -39,10 +54,32 @@ public class NotaCreditoController implements Serializable {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ResponseEntity<?> add(@RequestBody NotaCredito notaCredito) {
+    public ResponseEntity<?> add(OAuth2Authentication authentication,
+            @RequestBody NotaCredito notaCredito) {
+        Usuarios user = usuariosDAO.findUsuarioByUsername(authentication.getName());
+        notaCredito.setUsuarioCreacion(user.getIdUsuario());
+        notaCredito.setFechaCreacion(new Date());
+        notaCredito.setEstadoUso("SIN USO");
         notaCreditoDAO.add(notaCredito);
-        
-        return new ResponseEntity<>(HttpStatus.OK);
+        JsonResponse msg = new JsonResponse("Success", "Nota de credito agregada con exito");
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ResponseEntity<?> update(OAuth2Authentication authentication,
+            @RequestBody NotaCredito notaCredito) {
+        Usuarios user = usuariosDAO.findUsuarioByUsername(authentication.getName());
+        notaCredito.setUsuarioModificacion(user.getIdUsuario());
+        notaCredito.setFechaModificacion(new Date());
+        BigDecimal montoUpdate = new BigDecimal(BigInteger.ZERO);
+        List<DetalleNotaCredito> list = detalleNotaCreditoDAO.getByNotaCredito(notaCredito.getIdNotaCredito());
+        for (DetalleNotaCredito detalleNotaCredito : list) {
+            montoUpdate = montoUpdate.add(detalleNotaCredito.getMonto());
+        }
+        notaCredito.setMontoTotal(montoUpdate);
+        notaCreditoDAO.update(notaCredito);
+        JsonResponse msg = new JsonResponse("Success", "Nota de credito modificada con exito");
+        return new ResponseEntity<>(msg, HttpStatus.OK);
     }
 
 }
