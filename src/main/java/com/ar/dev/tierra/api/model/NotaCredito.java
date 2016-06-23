7c77c4dc49f80e3a06b6b5439624dcc0c6d61d29
Paulo Galdo <hades_54st@hotmail.com>
2016-06-23 03:53:24 -0300

    modelo para notas de credito

---
+++ src/main/java/com/ar/dev/tierra/api/model/NotaCredito.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ar.dev.tierra.api.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author PauloGaldo
+ */
+@Entity
+@Table(name = "categoria")
+public class NotaCredito implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_nota_credito", unique = true, nullable = false)
+    private int idNotaCredito;
+
+    @Column(name = "numero", nullable = false)
+    private String numero;
+
+    @Column(name = "estado_uso", nullable = false)
+    private String estadoUso;
+
+    @Column(name = "monto_total", precision = 10, scale = 2)
+    private BigDecimal montoTotal;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_creacion", nullable = false, length = 13)
+    private Date fechaCreacion;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_modificacion", length = 13)
+    private Date fechaModificacion;
+
+    @Column(name = "usuario_creacion", nullable = false)
+    private int usuarioCreacion;
+
+    @Column(name = "usuario_modificacion")
+    private Integer usuarioModificacion;
+
+    @Column(name = "id_cliente")
+    private Integer idCliente;
+
+    public NotaCredito() {
+    }
+
+    public NotaCredito(int idNotaCredito, String numero, String estadoUso, BigDecimal montoTotal, Date fechaCreacion, Date fechaModificacion, int usuarioCreacion, Integer usuarioModificacion, Integer idCliente) {
+        this.idNotaCredito = idNotaCredito;
+        this.numero = numero;
+        this.estadoUso = estadoUso;
+        this.montoTotal = montoTotal;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaModificacion = fechaModificacion;
+        this.usuarioCreacion = usuarioCreacion;
+        this.usuarioModificacion = usuarioModificacion;
+        this.idCliente = idCliente;
+    }
+
+    /**
+     * @return the idNotaCredito
+     */
+    public int getIdNotaCredito() {
+        return idNotaCredito;
+    }
+
+    /**
+     * @param idNotaCredito the idNotaCredito to set
+     */
+    public void setIdNotaCredito(int idNotaCredito) {
+        this.idNotaCredito = idNotaCredito;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the estadoUso
+     */
+    public String getEstadoUso() {
+        return estadoUso;
+    }
+
+    /**
+     * @param estadoUso the estadoUso to set
+     */
+    public void setEstadoUso(String estadoUso) {
+        this.estadoUso = estadoUso;
+    }
+
+    /**
+     * @return the montoTotal
+     */
+    public BigDecimal getMontoTotal() {
+        return montoTotal;
+    }
+
+    /**
+     * @param montoTotal the montoTotal to set
+     */
+    public void setMontoTotal(BigDecimal montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+
+    /**
+     * @return the fechaCreacion
+     */
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    /**
+     * @param fechaCreacion the fechaCreacion to set
+     */
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    /**
+     * @return the fechaModificacion
+     */
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    /**
+     * @param fechaModificacion the fechaModificacion to set
+     */
+    public void setFechaModificacion(Date fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    /**
+     * @return the usuarioCreacion
+     */
+    public int getUsuarioCreacion() {
+        return usuarioCreacion;
+    }
+
+    /**
+     * @param usuarioCreacion the usuarioCreacion to set
+     */
+    public void setUsuarioCreacion(int usuarioCreacion) {
+        this.usuarioCreacion = usuarioCreacion;
+    }
+
+    /**
+     * @return the usuarioModificacion
+     */
+    public Integer getUsuarioModificacion() {
+        return usuarioModificacion;
+    }
+
+    /**
+     * @param usuarioModificacion the usuarioModificacion to set
+     */
+    public void setUsuarioModificacion(Integer usuarioModificacion) {
+        this.usuarioModificacion = usuarioModificacion;
+    }
+
+    /**
+     * @return the idCliente
+     */
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    /**
+     * @param idCliente the idCliente to set
+     */
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+}
