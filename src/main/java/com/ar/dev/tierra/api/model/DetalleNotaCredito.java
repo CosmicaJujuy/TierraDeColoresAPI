62d8771c4f5b0bfa0f867ffb2b9527e90f6215f2
Paulo Galdo <hades_54st@hotmail.com>
2016-06-23 03:53:35 -0300

    modelo para el cuerpo de las notas de credito

---
+++ src/main/java/com/ar/dev/tierra/api/model/DetalleNotaCredito.java
@@ -0,0 +1,206 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ar.dev.tierra.api.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author PauloGaldo
+ */
+@Entity
+@Table(name = "detalle_nota_credito")
+public class DetalleNotaCredito implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_detalle_nota_credito", unique = true, nullable = false)
+    private int idDetalleNotaCredito;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_producto", nullable = false)
+    private Producto producto;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_factura", nullable = false)
+    private Factura factura;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_nota_credito", nullable = false)
+    private NotaCredito notaCredito;
+
+    @Column(name = "monto", precision = 10, scale = 2)
+    private BigDecimal monto;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_creacion", nullable = false, length = 13)
+    private Date fechaCreacion;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_modificacion", length = 13)
+    private Date fechaModificacion;
+
+    @Column(name = "usuario_creacion", nullable = false)
+    private int usuarioCreacion;
+
+    @Column(name = "usuario_modificacion")
+    private Integer usuarioModificacion;
+
+    public DetalleNotaCredito() {
+    }
+
+    public DetalleNotaCredito(int idDetalleNotaCredito, Producto producto, Factura factura, NotaCredito notaCredito, BigDecimal monto, Date fechaCreacion, Date fechaModificacion, int usuarioCreacion, Integer usuarioModificacion) {
+        this.idDetalleNotaCredito = idDetalleNotaCredito;
+        this.producto = producto;
+        this.factura = factura;
+        this.notaCredito = notaCredito;
+        this.monto = monto;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaModificacion = fechaModificacion;
+        this.usuarioCreacion = usuarioCreacion;
+        this.usuarioModificacion = usuarioModificacion;
+    }
+
+    /**
+     * @return the idDetalleNotaCredito
+     */
+    public int getIdDetalleNotaCredito() {
+        return idDetalleNotaCredito;
+    }
+
+    /**
+     * @param idDetalleNotaCredito the idDetalleNotaCredito to set
+     */
+    public void setIdDetalleNotaCredito(int idDetalleNotaCredito) {
+        this.idDetalleNotaCredito = idDetalleNotaCredito;
+    }
+
+    /**
+     * @return the producto
+     */
+    public Producto getProducto() {
+        return producto;
+    }
+
+    /**
+     * @param producto the producto to set
+     */
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    /**
+     * @return the factura
+     */
+    public Factura getFactura() {
+        return factura;
+    }
+
+    /**
+     * @param factura the factura to set
+     */
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    /**
+     * @return the notaCredito
+     */
+    public NotaCredito getNotaCredito() {
+        return notaCredito;
+    }
+
+    /**
+     * @param notaCredito the notaCredito to set
+     */
+    public void setNotaCredito(NotaCredito notaCredito) {
+        this.notaCredito = notaCredito;
+    }
+
+    /**
+     * @return the monto
+     */
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    /**
+     * @param monto the monto to set
+     */
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
+
+    /**
+     * @return the fechaCreacion
+     */
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    /**
+     * @param fechaCreacion the fechaCreacion to set
+     */
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    /**
+     * @return the fechaModificacion
+     */
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    /**
+     * @param fechaModificacion the fechaModificacion to set
+     */
+    public void setFechaModificacion(Date fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    /**
+     * @return the usuarioCreacion
+     */
+    public int getUsuarioCreacion() {
+        return usuarioCreacion;
+    }
+
+    /**
+     * @param usuarioCreacion the usuarioCreacion to set
+     */
+    public void setUsuarioCreacion(int usuarioCreacion) {
+        this.usuarioCreacion = usuarioCreacion;
+    }
+
+    /**
+     * @return the usuarioModificacion
+     */
+    public Integer getUsuarioModificacion() {
+        return usuarioModificacion;
+    }
+
+    /**
+     * @param usuarioModificacion the usuarioModificacion to set
+     */
+    public void setUsuarioModificacion(Integer usuarioModificacion) {
+        this.usuarioModificacion = usuarioModificacion;
+    }
+
+}
