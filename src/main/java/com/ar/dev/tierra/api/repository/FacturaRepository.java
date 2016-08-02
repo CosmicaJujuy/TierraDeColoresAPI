b37f659def4864b07fbbf1715198baa14ff78c66
PauloGaldo <PauloGaldo@192.168.1.53>
2016-08-02 12:02:10 -0300

    orden descendiente para la paginacion

--- src/main/java/com/ar/dev/tierra/api/repository/FacturaRepository.java
+++ src/main/java/com/ar/dev/tierra/api/repository/FacturaRepository.java
@@ -17,8 +17,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  */
 public interface FacturaRepository extends PagingAndSortingRepository<Factura, String> {
 
-    Page<Factura> findByEstadoNotAndFechaCreacionBetweenDesc(String estado, Date from, Date to, Pageable pageable);
+    Page<Factura> findByEstadoNotAndFechaCreacionBetweenAndOrderByFacturaIdDesc(String estado, Date from, Date to, Pageable pageable);
 
-    Page<Factura> findByEstadoAndFechaCreacionBetweenDesc(String estado, Date from, Date to, Pageable pageable);
+    Page<Factura> findByEstadoAndFechaCreacionBetweenAndOrderByFacturaIdDesc(String estado, Date from, Date to, Pageable pageable);
 
 }
