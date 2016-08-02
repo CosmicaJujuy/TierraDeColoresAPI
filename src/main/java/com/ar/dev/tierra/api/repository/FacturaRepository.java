38ade35005a665391097b2eeca6717931c461aa9
PauloGaldo <PauloGaldo@192.168.1.53>
2016-08-02 11:47:55 -0300

    repositorio para paginacion personalizada

---
+++ src/main/java/com/ar/dev/tierra/api/repository/FacturaRepository.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ar.dev.tierra.api.repository;
+
+import com.ar.dev.tierra.api.model.Factura;
+import java.util.Date;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ *
+ * @author PauloGaldo
+ */
+public interface FacturaRepository extends PagingAndSortingRepository<Factura, String> {
+
+    Page<Factura> findByEstadoNotAndFechaCreacionBetween(String estado, Date from, Date to, Pageable pageable);
+
+    Page<Factura> findByEstadoAndFechaCreacionBetween(String estado, Date from, Date to, Pageable pageable);
+
+}
