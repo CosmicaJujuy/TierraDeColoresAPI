54307e6dffe0d0bee34ac470a854f85249d304db
Paulo <Paulo@Asus>
2017-07-25 00:18:10 -0300

    Query optimizada para mejro carga de listado de facturas

--- src/main/java/com/ar/dev/tierra/api/repository/FacturaRepository.java
+++ src/main/java/com/ar/dev/tierra/api/repository/FacturaRepository.java
@@ -6,6 +6,7 @@
 package com.ar.dev.tierra.api.repository;
 
 import com.ar.dev.tierra.api.model.Factura;
+import com.ar.dev.tierra.api.model.FacturaProjection;
 import java.math.BigDecimal;
 import java.util.Date;
 import org.springframework.data.domain.Page;
@@ -20,13 +21,27 @@ import org.springframework.data.repository.query.Param;
  */
 public interface FacturaRepository extends Repository<Factura, String> {
 
-    @Query("SELECT f FROM Factura f "
-            + "WHERE f.estado <> 'RESERVADO' AND "
-            + "(f.idSucursal = :idSucursal) AND "
-            + "f.fechaCreacion BETWEEN "
+    @Query("SELECT NEW com.ar.dev.tierra.api.model.FacturaProjection( "
+            + "F.idFactura, "
+            + "F.estado, "
+            + "C.nombreCliente, "
+            + "F.total, "
+            + "F.fechaCreacion, "
+            + "U.idUsuario, "
+            + "U.nombre, "
+            + "U.apellido, "
+            + "F.numeracion, "
+            + "F.regalo) "
+            + "FROM Factura F "
+            + "LEFT JOIN F.cliente C "
+            + "LEFT JOIN F.idVendedor U "            
+            + "WHERE F.estado <> 'RESERVADO' "
+            + "AND F.idSucursal = :idSucursal "
+            + "AND F.fechaCreacion BETWEEN "
             + ":from AND :to "
-            + "ORDER BY f.idFactura DESC")
-    Page<Factura> findFacturasByDate(@Param("from") Date from, @Param("to") Date to, Pageable pageable, @Param("idSucursal") int idSucursal);
+            + "ORDER BY F.idFactura DESC"
+    )
+    Page<FacturaProjection> findFacturasByDate(@Param("from") Date from, @Param("to") Date to, Pageable pageable, @Param("idSucursal") int idSucursal);
 
     @Query("SELECT f FROM Factura f "
             + "WHERE f.estado = 'RESERVADO' AND "
